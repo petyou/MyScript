2478e2e52542c04e0165af16665f75755b942326
petyou <812607796@qq.com>
2019-02-20 19:07:28 +0800

    feat: 自动生成property

--- auto_create_factory_method.py
+++ auto_create_factory_method.py
@@ -1,18 +1,100 @@
 import os
+import re
+import argparse
 
+strong_type = ['copy', 'strong', 'retain']
 
 def codeCreate(hPath):
     data = open(hPath, "r", encoding="utf-8", errors="ignore")
     each = data.readline()
-    print(data)
 
+    my_property = list()
+    whole_string = ''
     while each:
-        print(each)
+        whole_string += each
+
+        if each.find("@property") != -1:
+            clean_each = each.strip("\n")
+            my_property.append(each)
+
         each = data.readline()
 
 
+    factory_string = "+ (instancetype)itemWith"
 
+    for index in range(len(my_property)):
+        each = my_property[index]
+        match = re.findall(r"\b[a-zA-Z_0-9]+\b", each)
+        name = match[-1]
+        type = match[-2]
+        retain_type = match[-3]
+
+        if index == 0:
+            if retain_type in strong_type:
+                factory_string += '%s:(%s *)%s' % (name.capitalize(), type, name)
+            else:
+                factory_string += '%s:(%s)%s' % (name.capitalize(), type, name)
+
+        else:
+            if retain_type in strong_type:
+                factory_string += '\n%s:(%s *)%s' % (name, type, name)
+            else:
+                factory_string += '\n%s:(%s)%s' % (name, type, name)
+
+    factory_string += ';'
+    whole_string = whole_string.replace("@end", factory_string)
+    whole_string += "\n@end"
+
+    with open(hPath, 'w', encoding='utf-8', errors='ignore') as f:
+        f.write(whole_string)
+
+    class_name = ""
+    match = re.findall(r"\b[a-zA-Z_0-9]+\b", hPath)
+    class_name = match[-2]
+
+
+    ## .m 方法
+    mPath = hPath[0:-1]
+    mPath += "m"
+
+
+    data = open(mPath, "r", encoding="utf-8", errors="ignore")
+    each = data.readline()
+
+    whole_string = ''
+    while each:
+        whole_string += each
+        each = data.readline()
+
+    whole_string = whole_string.replace("@end", factory_string)
+    whole_string = whole_string.rstrip()
+    whole_string = whole_string[0:-1]
+    whole_string += '\n{\n'
+    whole_string += "\t%s* item = [self new];\n" % (class_name)
+
+    for index in range(len(my_property)):
+        each = my_property[index]
+        match = re.findall(r"\b[a-zA-Z_0-9]+\b", each)
+        name = match[-1]
+        type = match[-2]
+        whole_string += "\titem.%s = %s;\n" % (name, name)
+
+    whole_string += "\treturn item;\n}"
+    whole_string += "\n\n@end"
+
+    with open(mPath, 'w', encoding='utf-8', errors='ignore') as f:
+       f.write(whole_string)
 
 
 if __name__ == "__main__":
-    codeCreate(r"/Users/wen/tips-ios2/Liaodao/Classes/Follow/IndicialEquation/Main/Model/LDLeagueStandingItem.h")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path", "--path", help=".h的全路径", metavar="path")
+    options = parser.parse_args()
+    hPath = ""
+    hPath = options.path
+
+    if hPath:
+        codeCreate(hPath)
+        print("Done")
+    else:
+        print(".h路径为空 使用 -path带上路径")
---
+++ auto_property.py
@@ -0,0 +1,53 @@
+import os
+import re
+import argparse
+
+
+
+def codeCreate(hPath):
+    data = open(hPath, "r", encoding="utf-8", errors="ignore")
+    each = data.readline()
+
+    my_property_list = list()
+    whole_string = ''
+    while each:
+        whole_string += each
+
+        if each.find("=") != -1:
+            name = each.split("=")[0]
+            name = name.strip()
+            my_property_list.append(name)
+
+        each = data.readline()
+
+    property_string = ''
+
+    for i in range(len(my_property_list)):
+        each_property = my_property_list[i]
+        auto_property = "@property (nonatomic, copy) NSString *%s;\n" % (each_property)
+        property_string += auto_property
+
+    whole_string = whole_string.replace("@end", property_string)
+    whole_string += "\n@end"
+
+    with open(hPath, 'w', encoding='utf-8', errors='ignore') as f:
+        f.write(whole_string)
+
+    print("Done!!")
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-path", "--path", help=".h的全路径", metavar="path")
+    options = parser.parse_args()
+    hPath = ""
+    hPath = options.path
+
+    if hPath:
+        codeCreate(hPath)
+        print("Done")
+    else:
+        print(".h路径为空 使用 -path带上路径")
+
